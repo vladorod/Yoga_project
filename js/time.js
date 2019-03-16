a40bd5bcf081cd7810b464bcb5cd179d778273c0
Vlad <effectcor@gmail.com>
2019-03-16 17:10:58 +0300

    added

--- js/time.js
+++ js/time.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // Timer 
 
-  let deadline = "2019-03-21";
+  let deadline = "2019-03-18";
 
 function getTimeRemaining(endtime) { 
 
@@ -34,17 +34,78 @@ function setClock(id, endtime) {
   function updateClock() {
     let t = getTimeRemaining(endtime);
 
-    hours.textContent = t.hours;
-    minutes.textContent = t.minutes;
-    second.textContent = t.seconds;
+    hours.textContent = addZero(t.hours);
+    minutes.textContent = addZero(t.minutes);
+    second.textContent = addZero(t.seconds);
     
     if (t.total <= 0) { 
       clearInterval(timeInterval);
+      hours.textContent = "00";
+      minutes.textContent = "00";
+      second.textContent = "00";
     };
   };
 };
 
+function addZero(num) { 
+  if ( num < 10 ) { 
+    return num = '0' + num;
+  } else return num; 
+}
+
 setClock("timer", deadline)
 
+// Srcroll animation 
+
+function animate(options) {
+
+  let start = performance.now();
+
+  requestAnimationFrame(function animate(time) {
+    // timeFraction от 0 до 1
+    let timeFraction = (time - start) / options.duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    // текущее состояние анимации
+    let progress = options.timing(timeFraction)
+    
+    options.draw(progress);
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+
+  });
+}
+
+
+let a = document.querySelectorAll('a[href="#"]');
+  
+
+ 
+  // a.addEventListener('click', (e) => { 
+
+  //   e.preventDefault();
+
+  //   animate({
+  //     duration: 1000,
+  //     timing: function(timeFraction) {
+  //       return Math.pow(timeFraction, 5);
+  //     },
+  //     draw: function(progress) {
+  //      scrollTo(document.querySelector(href).getBoundingClientRect().top)
+  //     }
+  // });
+    
+  // });
+  
+ 
 });
 
+
+let arr = document.querySelectorAll('ul a[href]');
+
+for (var i = 0; i < arr.length; i++) {
+
+  console.log('asd')
+  }
