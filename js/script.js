33d358e71901093712c3d53856c714c63011c2b2
Vlad <effectcor@gmail.com>
2019-03-23 23:46:27 +0300

    added calc

--- js/script.js
+++ js/script.js
@@ -262,4 +262,45 @@ for (let i = 0; i < dots.length+1; i++) {
 }
 });
 
-  });
+//Calc
+
+let presons = document.querySelectorAll('.counter-block-input')[0],
+    restDays = document.querySelectorAll('.counter-block-input')[1],
+    place = document.querySelector('#select'),
+    totalValue = document.querySelector('#total'),
+    presonsSum = 0,
+    daysSum = 0,
+    total = 0;
+
+totalValue.innerHTML = 0;
+
+presons.addEventListener('input', function (e) {
+  if ( this.value != ""){
+  this.value = this.value.replace(/\D/g, "");
+  presonsSum= this.value; 
+  total = (daysSum + presonsSum)*4000;
+  if (restDays.value != '') {
+    totalValue.innerHTML = total;
+  } else totalValue.innerHTML = 0;
+} else totalValue.innerHTML = 0;
+});
+
+restDays.addEventListener('input', function (e) {
+  if ( this.value != ""){
+  this.value = this.value.replace(/\D/g, "");
+  daysSum=this.value 
+  total  = (daysSum + presonsSum)*4000;
+  if (presons.value != '') {
+    totalValue.innerHTML = total;
+  } else totalValue.innerHTML = 0;
+} else totalValue.innerHTML = 0;
+});
+
+place.addEventListener('change', function() {
+  if (presons != "" || restDays != "") { 
+    let a = total;
+    totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+  }
+});
+
+});
